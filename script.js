9b7e7cfd144aead3cc8a9c217e3581eb0d8a887f
Alisha (Jungah Sohn) <77492629+alisha0815@users.noreply.github.com>
2021-11-08 16:25:23 +0100

    initializing the starting condition- dice, score

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+// Setting up the starting condition
+// Initialize the score setting to 0 using ID
+
+// Selecting Elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+
+// Starting Conditions
+score0El.textContent = 0;
+score1El.textContent = 0; //JS will automatically convert them to strings to actually display them on the page
+// console.log(score0);
+// console.log(score1);
+
+// Hide the dice here
+// Create a hidden class (CSS) and then add that hidden
+diceEl.classList.add('hidden');
