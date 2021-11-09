db476cd4ca902ad12e170bd80c12b97d291915f7
Alisha (Jungah Sohn) <77492629+alisha0815@users.noreply.github.com>
2021-11-09 12:18:07 +0100

    creating global score,activating deactivating game

--- script.js
+++ script.js
@@ -4,6 +4,7 @@
 // Initialize the score setting to 0 using ID
 
 // Selecting Elements
+
 const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
 
@@ -20,6 +21,17 @@ let currentScore = 0; // initialize current score
 const scores = [0, 0];
 // keep track of the active player
 let activePlayer = 0; // we start with player #1
+let playing = true;
+
+// Switch to the next player
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent =
+    currentScore;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
 
 // Starting Conditions
 score0El.textContent = 0;
@@ -29,39 +41,63 @@ score1El.textContent = 0; //JS will automatically convert them to strings to act
 
 // Hide the dice here
 // Create a hidden class (CSS) and then add that hidden
-diceEl.classList.add('hidden');
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
-  // 1. Generate a random dice roll
-  const dice = Math.trunc(Math.random() * 6) + 1;
-  // console.log(dice);
+  // when playing is true
+  if (playing) {
+    // 1. Generate a random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    // console.log(dice);
 
-  // 2. Display dice
-  diceEl.classList.remove('hidden');
-  // console.log(diceEl.src);
-  diceEl.src = `dice-${dice}.png`;
+    // 2. Display dice
+    diceEl.classList.remove('hidden');
+    // console.log(diceEl.src);
+    diceEl.src = `dice-${dice}.png`;
 
-  // 3. Check for rolled 1
-  if (dice !== 1) {
-    // Add dice to the current score
-    currentScore += dice;
-    // console.log(currentScore);
-    // building id name dynamically!
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
-    console.log(currentScore);
-    // current0Ele.textContent = currentScore; // Change later
-  } else {
-    // previous players current score set to be visibly 0
-    currentScore = 0;
-    // reset the current score
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
-    // If true, switch to next player
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    // change background color of the active player
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
+    // 3. Check for rolled 1
+    if (dice !== 1) {
+      // Add dice to the current score
+      currentScore += dice;
+      // console.log(currentScore);
+      // building id name dynamically!
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+      console.log(currentScore);
+      // current0Ele.textContent = currentScore; // Change later
+    } else {
+      // previous players current score set to be visibly 0
+      switchPlayer();
+    }
+  }
+});
+
+// Holding the current score
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    // 1. Add current score to active player's score
+    scores[activePlayer] += currentScore;
+    // console.log('current', `player ${activePlayer}`, currentScore);
+    // display the global score
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+
+    // 2. Check if player's score is >= 100
+    if (scores[activePlayer] >= 20) {
+      // Finish the game
+      playing = false;
+      // Make the dice hidden
+      diceEl.classList.add('hidden');
+      console.log(`💯💯 Player ${activePlayer + 1} won!`);
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    } else {
+      // Switch to the next player
+      switchPlayer();
+    }
   }
 });
